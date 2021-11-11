88785cf6732cc4f4159edeec7fb5dd5a5d9431af
Alessandro Pisu <a.pisu@ilivetech.it>
2021-11-11 14:29:41 +0100

    📈 Add counter.dev analytics

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,6 @@
 export function Navbar() {
   return (
-    <div className="flex flex-row-reverse space-x-reverse space-x-10 px-4 py-2 text-white">
+    <div className="flex flex-row-reverse space-x-reverse space-x-12 px-4 py-2 text-white">
       <a
         href="https://github.com/alessandropisu"
         target="_blank"
@@ -10,15 +10,24 @@ export function Navbar() {
         <i className="nes-icon github" />
         <p className="ml-3 text-xs sm:text-base">My GitHub</p>
       </a>
-      <a
-        href="https://www.buymeacoffee.com/lofi.station"
-        target="_blank"
-        rel="noreferrer"
-        className="flex items-center justify-center hover:text-white"
-      >
-        <i className="nes-icon coin" />
-        <p className="ml-3 text-xs sm:text-base">Donate an espresso</p>
-      </a>
+      <div className="flex items-center justify-center hover:text-white">
+        <p className="mr-2 text-xs sm:text-base">Share on</p>
+        <a
+          href="https://www.facebook.com/sharer/sharer.php?u=soundsoftheworld.vercel.app"
+          target="_blank"
+          rel="noreferrer"
+          className="mr-2"
+        >
+          <i className="nes-icon facebook" />
+        </a>
+        <a
+          href="https://twitter.com/intent/tweet?url=soundsoftheworld.vercel.app&text="
+          target="_blank"
+          rel="noreferrer"
+        >
+          <i className="nes-icon twitter" />
+        </a>
+      </div>
     </div>
   );
 }
